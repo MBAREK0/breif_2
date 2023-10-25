dc061128feb15aba5b5323aff5db323cfe2a8618
MBAREK0 <elaadraouimbarek2023@gmail.com>
2023-10-25 20:36:18 +0100

    j'ai ajoute le button pour hidden list accsisoire

--- java.js
+++ java.js
@@ -1,7 +1,14 @@
 let openicon = document.querySelector("#sidebar_icon");
 let cart = document.querySelector("#sidbare_phone");
+let button_accs = document.querySelector("#button_list_Accessoires");
+let list = document.querySelector("#list_Accessoires_js");
 function myoppen() {
   cart.classList.toggle("mymbarek");
   openicon.classList.toggle("openicon");
 
 }
+function hid_accs() {
+    
+   list.classList.toggle("hidenn_list");
+  
+  }
