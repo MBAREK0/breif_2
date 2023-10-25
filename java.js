90d8e429f563dafbf3c67317c3f3feac84e39633
MBAREK0 <elaadraouimbarek2023@gmail.com>
2023-10-25 19:12:07 +0100

    j'ai ajout le sidebar bour affichier la serié (signin,flag,iconshop... aver utuliser js

---
+++ java.js
@@ -0,0 +1,7 @@
+let openicon = document.querySelector("#sidebar_icon");
+let cart = document.querySelector("#sidbare_phone");
+function myoppen() {
+  cart.classList.toggle("mymbarek");
+  openicon.classList.toggle("openicon");
+
+}
